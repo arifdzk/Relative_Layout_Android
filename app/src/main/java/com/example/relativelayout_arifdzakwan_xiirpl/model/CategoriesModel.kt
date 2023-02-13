9fb2f3876ae6819fb952b0c957d3451c1dc4c455
syahidazhar <syahidazharazizi@gmail.com>
2023-02-13 15:37:56 +0700

    Menambahkan android networking & adapter

---
+++ app/src/main/java/com/example/relativelayout_arifdzakwan_xiirpl/model/CategoriesModel.kt
@@ -0,0 +1,11 @@
+package com.example.relativelayout_arifdzakwan_xiirpl.model
+
+class CategoriesModel : java.io.Serializable{
+    @JvmField
+    var strCategory :String? = null
+
+    @JvmField
+    var strCategoryThumb: String? = null
+
+    var strCategoryDescription: String?=null
+}
